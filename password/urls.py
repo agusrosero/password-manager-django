a8f9786777c796d762e2fa264a381e478dd7e83d
agusrosero <agusrosero2000@gmail.com>
2024-03-06 13:58:01 -0300

    password add - views and html

--- password/urls.py
+++ password/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.password_list, name='password_list'),
+    path('add_password/', views.password_add, name='password_add'),
 ]
--- password/views.py
+++ password/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Entry
 
 
 def password_list(request):
     passwords = Entry.objects.all()
     return render(request, 'entry/password_list.html', {'passwords': passwords})
+
+
+def password_add(request):
+    if not request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        try:
+            name = request.POST['name']
+            url = request.POST['url']
+            username = request.POST['username']
+            password = request.POST['password']
+            Entry.objects.create(name=name, url=url,
+                                 username=username, password=password)
+            messages.success(request, f"New password added for {name}")
+        except Exception as err:
+            print('Error: ', err)
+    return render(request, 'entry/password_add.html')
