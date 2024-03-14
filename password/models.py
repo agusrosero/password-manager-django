ddba2ea43f5db251764badddc74e30f4db7e0df1
agusrosero <agusrosero2000@gmail.com>
2024-03-14 09:04:00 -0300

    add user-models

--- password/models.py
+++ password/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.models import User
 
 
 class Entry(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     url = models.URLField()
     username = models.CharField(max_length=100)
